6381b43ccc64047ff8314e9eb65dc888464cf9a9
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-01 19:54:16 +0200

    comm

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let imeIPrezimeInput = document.getElementById('iip')
 let regInput = document.getElementById('reg')
 let selectElement = document.getElementById('location')
 let box4 = document.getElementById('box4')
-let uniqueCode = 2
 let presevo = db.collection('Presevo')
 let elementForPrintVaucher = document.createElement('div')
 
@@ -13,34 +12,55 @@ let elementForPrintVaucher = document.createElement('div')
 
 // Create SET
 buttonForCreateVaucher.addEventListener('click', ()=>{
-    let selectedLocation = selectElement.options[selectElement.selectedIndex];
-    uniqueCode++
-    let cust = {
-        jmbg: jmbgInput.value,
-        imeIPrezime: imeIPrezimeInput.value,
-        regOznaka: regInput.value,
-        lokacija: selectedLocation.value,
-        jedinstveniKod: uniqueCode,
-        njegoveStranke: {
-            regOznakaStranke: {},
-        }
+    uniqueCodeFunc = () => {
+       let date = new Date()
+       let day = date.getDay()
+       let month = date.getMonth()+1
+       let year = date.getFullYear()
+       let hours = date.getHours()
+       let minutes = date.getMinutes()
+       let milisekundes = date.getMilliseconds()
+       
+       return `${day}${month}${year}${hours}${minutes}${milisekundes}`;    
     }
+    let divMsg = document.createElement('div')
+    box4.appendChild(divMsg)
+    let uniqueCode = uniqueCodeFunc()
     
-    db.collection('Presevo').doc(`${cust.jmbg}`)
-    .set(cust)
-    .then(() => {
-        let divMsg = document.createElement('div')
-        divMsg.innerHTML = `${imeIPrezimeInput.value} uspesno dodat u bazu`
-        box4.appendChild(divMsg)
-        jmbgInput.value = ''
-        imeIPrezimeInput.value = ''
-        regInput.value = ''
-        selectedLocation.value = ''
-    })
-    .catch((e)=>{
-        console.log(`Desila se greska ${e}`);
-    })
+    if(jmbgInput.value.length!=13){
+        console.log('da');
+        divMsg.innerHTML = 'PROVERI JMBG'
+    } else if (regInput.value.length <7){
+        divMsg.innerHTML = 'PROVERI REG.OZNAKU'
+    } else {
+        let selectedLocation = selectElement.options[selectElement.selectedIndex];
+        let cust = {
+            jmbg: jmbgInput.value,
+            imeIPrezime: imeIPrezimeInput.value,
+            regOznaka: regInput.value,
+            lokacija: selectedLocation.value,
+            jedinstveniKod: uniqueCode,
+            njegoveStranke: {
+                regOznakaStranke: {},
+            }
+        }
+        
+        db.collection(`${selectedLocation.value}`).doc(`${cust.jmbg}`)
+        .set(cust)
+        .then(() => {
+            divMsg.innerHTML = `${imeIPrezimeInput.value} uspesno dodat u bazu`
+            jmbgInput.value = ''
+            imeIPrezimeInput.value = ''
+            regInput.value = ''
+            selectedLocation.value = ''
+        })
+        .catch((e)=>{
+            console.log(`Desila se greska ${e}`);
+        })
+    }
 })
+
+    
 
 
 

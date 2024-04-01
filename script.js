742d699a43b8a8e9a2d6d5c7f27cbb52cab2e9f3
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-01 17:32:42 +0200

    creating vaucher done

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
-let createVaucherButton = document.getElementById('create-vaucher-button')
-createVaucherButton.addEventListener('click', ()=>{
-    console.log('da');
+let buttonForCreateVaucher = document.getElementById('create-vaucher-button')
+let jmbgInput = document.getElementById('input-jmbg')
+let imeIPrezimeInput = document.getElementById('iip')
+let regInput = document.getElementById('reg')
+let selectElement = document.getElementById('location')
+let box4 = document.getElementById('box4')
+let uniqueCode = 2
+let presevo = db.collection('Presevo')
+let elementForPrintVaucher = document.createElement('div')
+
+
+
+
+// Create SET
+buttonForCreateVaucher.addEventListener('click', ()=>{
+    let selectedLocation = selectElement.options[selectElement.selectedIndex];
+    uniqueCode++
+    let cust = {
+        jmbg: jmbgInput.value,
+        imeIPrezime: imeIPrezimeInput.value,
+        regOznaka: regInput.value,
+        lokacija: selectedLocation.value,
+        jedinstveniKod: uniqueCode,
+        njegoveStranke: {
+            regOznakaStranke: {},
+        }
+    }
+    
+    db.collection('Presevo').doc(`${cust.jmbg}`)
+    .set(cust)
+    .then(() => {
+        let divMsg = document.createElement('div')
+        divMsg.innerHTML = `${imeIPrezimeInput.value} uspesno dodat u bazu`
+        box4.appendChild(divMsg)
+        jmbgInput.value = ''
+        imeIPrezimeInput.value = ''
+        regInput.value = ''
+        selectedLocation.value = ''
+    })
+    .catch((e)=>{
+        console.log(`Desila se greska ${e}`);
+    })
 })
-console.log(db);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*                    ___
+doc.set -> create        |
+doc.get -> read           \ _ Vracaju nam promise.
+doc.update -> update      /   Nakon ovih poziva 
+doc.delete -> delete  ___|    lancaju se .then() i .catch()
+*/
